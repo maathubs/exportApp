9a2cc729a893eaf84fcf30cfadeb159082cbf21b
maathubs <mathusudarsasn@gmail.com>
2019-09-27 11:41:03 +0530

    port Included

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ res.sendFile(filePath);
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(5000, () => console.log(`Example app listening on port 5000!`))
+app.listen(process.env.PORT || 5000, () => console.log(`Example app listening on port 5000!`))
